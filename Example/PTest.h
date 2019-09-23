256c4f5af334cb0d9f6c1cd669a4ce97dc1f5cb2
qiujudou <447258925@qq.com>
2019-09-23 23:28:11 +0800

    0.1.1

---
+++ Example/PTest.h
@@ -0,0 +1,17 @@
+//
+//  PTest.h
+//  MyPrivateItem_Example
+//
+//  Created by qiujudou on 2019/9/23.
+//  Copyright © 2019年 447258925@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/PTest.m
@@ -0,0 +1,13 @@
+//
+//  PTest.m
+//  MyPrivateItem_Example
+//
+//  Created by qiujudou on 2019/9/23.
+//  Copyright © 2019年 447258925@qq.com. All rights reserved.
+//
+
+#import "PTest.h"
+
+@implementation PTest
+
+@end
